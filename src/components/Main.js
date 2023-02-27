eb49825dbe8950d9efc2b8d8f06ad27b32bb1592
Наталья <mrs.solts@mail.ru>
2023-02-27 22:51:31 +0300

    feat: добавить императивные обработчики внутри компонента Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 function Main() {
+  const handleEditAvatarClick = () => {
+    const popupEditAvatar = document.querySelector('.popup_type_update-avatar');
+    popupEditAvatar.classList.add('popup_opened');
+  }
+  const handleEditProfileClick = () => {
+    const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+    popupEditProfile.classList.add('popup_opened');
+    }
+  const handleAddPlaceClick = () => {
+    const popupAddPlace = document.querySelector('.popup_type_add-place');
+    popupAddPlace.classList.add('popup_opened');
+   }
+
+
     return (
         <main className="content">
             <section className="profile">
-            <button className="profile__avatar-button">
+            <button className="profile__avatar-button" onClick={handleEditAvatarClick}>
                 <img src="#" alt="Аватар пользователя" className="profile__avatar" />
             </button>
             <div className="profile__info">
                 <h1 className="profile__name" />
-                <button className="profile__edit-button button-hover" type="button" />
+                <button className="profile__edit-button button-hover" type="button" onClick={handleEditProfileClick} />
                 <p className="profile__about" />
             </div>
-            <button className="profile__add-button button-hover" type="button" />
+            <button className="profile__add-button button-hover" type="button" onClick={handleAddPlaceClick} />
             </section>
             <section className="cards" aria-label="Карточки с изображением мест">
                 <ul className="cards__list"></ul>
