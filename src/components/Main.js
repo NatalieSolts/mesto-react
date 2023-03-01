05124274182bdfa1bcf6c582648d5f58c835e45d
Наталья <mrs.solts@mail.ru>
2023-03-02 01:05:29 +0300

    feat: подключить API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../utils/api';
 
 function Main( {onEditProfile, onAddPlace, onEditAvatar} ) {
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
 
-  // const handleEditAvatarClick = () => {
-  //   const popupEditAvatar = document.querySelector('.popup_type_update-avatar');
-  //   popupEditAvatar.classList.add('popup_opened');
-  // }
-  // const handleEditProfileClick = () => {
-  //   const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-  //   popupEditProfile.classList.add('popup_opened');
-  //   }
-  // const handleAddPlaceClick = () => {
-  //   const popupAddPlace = document.querySelector('.popup_type_add-place');
-  //   popupAddPlace.classList.add('popup_opened');
-  //  }
-
+  useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      .then(([userRes, cardsRes]) => {
+        setUserName(userRes.name)
+        setUserDescription(userRes.about)
+        setUserAvatar(userRes.avatar)
+        // setCards(cardsRes)
+      })
+      .catch((err) => console.log(err))
+  }, [])
 
     return (
         <main className="content">
             <section className="profile">
-            <button className="profile__avatar-button" onClick={onEditAvatar}>
-                <img src="#" alt="Аватар пользователя" className="profile__avatar" />
+            <button className="profile__avatar-button" onClick={onEditAvatar} >
+                <img src={userAvatar} alt="Аватар пользователя" className="profile__avatar" />
             </button>
             <div className="profile__info">
-                <h1 className="profile__name" />
+                <h1 className="profile__name">{userName}</h1>
                 <button className="profile__edit-button button-hover" type="button" onClick={onEditProfile} />
-                <p className="profile__about" />
+                <p className="profile__about">{userDescription}</p>
             </div>
             <button className="profile__add-button button-hover" type="button" onClick={onAddPlace} />
             </section>
