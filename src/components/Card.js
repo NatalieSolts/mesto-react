4e5ee13fe1bc6cb8db57da48a8edd40f6c1cf7c7
Наталья <mrs.solts@mail.ru>
2023-03-12 00:19:36 +0300

    feat: подписать на контекст компонент Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Card( {card, onCardClick} ) {
+function Card({ card, onCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+  // Определяем, являемся ли мы владельцем текущей карточки:
+  const isOwn = card.owner._id === currentUser._id;
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = `card__like-button ${
+    isLiked && "card__like-button_active"
+  }`;
+
   function handleClick() {
     onCardClick(card);
   }
 
-  return(
+  return (
     <li className="cards__card">
       <img
         className="cards__image"
@@ -13,22 +25,22 @@ function Card( {card, onCardClick} ) {
         alt={card.name}
         onClick={handleClick}
       />
-      <button
-        className="cards__delete button-hover"
-        type="button">
-      </button>
+      {/* используем переменную для условного рендеринга */}
+      {isOwn && (
+        <button className="cards__delete button-hover" type="button"></button>
+      )}
       <div className="cards__description">
         <h2 className="cards__name">{card.name}</h2>
         <div className="cards__like-group">
           <button
             className="cards__icon-heart button-hover"
-            type="button">
-          </button>
+            type="button"
+          ></button>
           <p className="cards__number-of-likes">{card.likes.length}</p>
         </div>
       </div>
     </li>
-  )
+  );
 }
 
 export default Card;
