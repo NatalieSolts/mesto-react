601da11c6416ea3da1f9d66c93abb7d42b1b10b8
Наталья <mrs.solts@mail.ru>
2023-03-12 00:06:23 +0300

    feat: использовать контекст в Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import api from '../utils/api';
-import Card from './Card';
+import React, { useState, useEffect, useContext } from "react";
+import api from "../utils/api";
+import Card from "./Card";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Main( {onEditProfile, onAddPlace, onEditAvatar, onCardClick} ) {
-  const [userName, setUserName] = useState('');
-  const [userDescription, setUserDescription] = useState('');
-  const [userAvatar, setUserAvatar] = useState('');
+function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
   const [cards, setCards] = useState([]);
 
+  const currentUser = useContext(CurrentUserContext);
+
   useEffect(() => {
-    Promise.all([api.getUserInfo(), api.getInitialCards()])
-      .then(([userRes, cardsRes]) => {
-        setUserName(userRes.name);
-        setUserDescription(userRes.about);
-        setUserAvatar(userRes.avatar);
+    api
+      .getInitialCards()
+      .then((cardsRes) => {
         setCards(cardsRes);
       })
       .catch((err) => console.log(err));
@@ -22,21 +20,33 @@ function Main( {onEditProfile, onAddPlace, onEditAvatar, onCardClick} ) {
   return (
     <main className="content">
       <section className="profile">
-        <button className="profile__avatar-button" onClick={onEditAvatar} >
-            <img src={userAvatar} alt="Аватар пользователя" className="profile__avatar" />
+        <button className="profile__avatar-button" onClick={onEditAvatar}>
+          <img
+            src={currentUser.avatar}
+            alt="Аватар пользователя"
+            className="profile__avatar"
+          />
         </button>
         <div className="profile__info">
-          <h1 className="profile__name">{userName}</h1>
-          <button className="profile__edit-button button-hover" type="button" onClick={onEditProfile} />
-          <p className="profile__about">{userDescription}</p>
+          <h1 className="profile__name">{currentUser.name}</h1>
+          <button
+            className="profile__edit-button button-hover"
+            type="button"
+            onClick={onEditProfile}
+          />
+          <p className="profile__about">{currentUser.about}</p>
         </div>
-        <button className="profile__add-button button-hover" type="button" onClick={onAddPlace} />
+        <button
+          className="profile__add-button button-hover"
+          type="button"
+          onClick={onAddPlace}
+        />
       </section>
       <section className="cards" aria-label="Карточки с изображением мест">
         <ul className="cards__list">
           {cards.map((card) => (
             <Card
-            card={card}
+              card={card}
               // title={card.name}
               // likes={card.likes.length}
               // link={card.link}
